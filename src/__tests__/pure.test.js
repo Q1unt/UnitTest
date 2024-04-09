16f511d789a2354c90fbf0d4a8878800d5f609e5
Q1unt <dima.kunitsien.03@gmail.com>
2024-04-09 23:28:17 +0300

    initial commit

---
+++ src/__tests__/pure.test.js
@@ -0,0 +1,22 @@
+import { pure } from '../Pure.js'
+
+test("test charecter health", () => {
+    let character = {name: 'Маг', health: 90}
+    const result = pure(character);
+    expect(result).toBe('health');
+
+});
+
+test("test charecter wounded", () => {
+    let character = {name: 'Маг', wounded: 90}
+    const result = pure(character);
+    expect(result).toBe('wounded');
+
+});
+
+test("test charecter critical", () => {
+    let character = {name: 'Маг', critical: 90}
+    const result = pure(character);
+    expect(result).toBe('critical');
+
+});
---
+++ src/pure.js
@@ -0,0 +1,5 @@
+export function pure(character){
+        let result = Object.keys(character);
+        return result[1]
+};
+
